0a3795baef4b2738b5784f9190fff1eb9ba74532
Vladimir <vladimirverevkin63@gmail.com>
2022-08-22 01:55:48 +0700

    V1.0

--- ESP8266/include/main.h
+++ ESP8266/include/main.h
@@ -3,6 +3,7 @@
 //------------------------------------------------
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
 
 #include "nvmemory.h"
 #include "web/html.h"
--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -1,13 +1,11 @@
 //#include <Arduino.h>
 #include "main.h"
 
-/* Установите здесь свои SSID и пароль */
-//const char* ssid = "Keenetic-6621";  // SSID
-//const char* password = "DL4x4fBh"; // пароль
-
 /* Установка SSID и пароль сети в AP(Access Point) режиме - точка доступа*/
 const char* ssidAP = "ServiceRC";  // SSID
 const char* passwordAP = "123456789"; // пароль
+/* Переменые имя\IP адрес ПК и порт*/
+
 
 /* Настройки IP адреса */
 IPAddress local_ip(192,168,4,22);
@@ -21,17 +19,30 @@ void handle_settings();
 void handle_send();
 void handle_reset();
 void handle_NotFound();
+void handleBody();
 
+void handle_Led_ON(); // для теста
+void handle_Led_OFF();// для теста
+
+StaticJsonDocument<256> jsonDoc; // ArduinoJson 6
+
+// процедура инициализации выводов
+void init_Pin(){
+  pinMode(D7, OUTPUT);
+}
+//==================================================================
 void setup() {
   // put your setup code here, to run once:
   uint8_t timeconnect=0; //переменная для счета попыток подключения
   Serial.begin(115200);
   delay(500);
   
+  init_Pin();
   // инициализация доступа к памяти для хранения данных(эн-память)
   initEEPROM();
   String ssid = readEEPROM(addressIDwifi);
   String password = readEEPROM(addressPASwifi);
+
   
   Serial.println("Connecting to ");
   Serial.println(ssid);
@@ -68,7 +79,13 @@ void setup() {
   //server.on("/send", handle_send);
   server.on("/settings", handle_settings);
   server.on("/reset", handle_reset);
+
+  server.on("/body", handleBody); //Associate the handler function to the pat
+
   server.onNotFound(handle_NotFound);
+
+  server.on("/on", handle_Led_ON);  //для теста
+  server.on("/off", handle_Led_OFF); //для теса
 
   server.begin();
   Serial.println("HTTP server started");
@@ -104,4 +121,41 @@ void handle_reset(){
 void handle_NotFound()
 {
   server.send(404, "text/plain", "Not found");
+}
+
+// для теста
+void handle_Led_ON()
+{
+  digitalWrite(D7, HIGH);// зажечь светодиод
+  server.send(200,"text/html", "Led_ON");
+}
+void handle_Led_OFF()
+{
+  digitalWrite(D7, LOW);// погасить светодиод
+  server.send(200,"text/html", "Led_OFF");
+}
+
+void handleBody() { //Handler for the body path
+ 
+      if (server.hasArg("plain")== false){ //Check if body received
+ 
+            server.send(200, "text/plain", "Body not received");
+            return;
+ 
+      }
+ 
+      String message = server.arg("plain");
+
+      DeserializationError error = deserializeJson(jsonDoc, message);// ArduinoJson 6
+      if(error){
+        Serial.print("deserializeJson() failed with code");
+        Serial.println(error.c_str());
+        return;
+      }
+      int ain = jsonDoc["a"];
+      String bin = jsonDoc["b"];
+      Serial.println(bin);
+
+      server.send(200, "application/json", message); // Отправляем ответ клиенту(формат json)
+      Serial.println(message);
 }
